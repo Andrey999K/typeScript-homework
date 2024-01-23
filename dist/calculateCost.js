af6b3f45045183ad53b83a501ab7b1e1cf9dff98
Andrey999K <andrey1234xxxx@mail.ru>
2024-01-23 03:54:34 +0300

    feat: add function for calculate cost

---
+++ dist/calculateCost.js
@@ -0,0 +1,6 @@
+"use strict";
+const totalPrice = ({ price, discount, isInstallment, months }) => {
+    return price * (100 - discount) / 100 / (isInstallment ? months : 1);
+};
+const price = totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 });
+console.log(price);
